89f614aae779dea380720e89d6dc7f37bcfaa667
tigelah <lokoportitimao2009@hotmail.com>
2020-01-21 13:09:45 -0300

    Events Stream

--- src/main/java/br/com/rodrigo/webflux/controllers/PlaylistController.java
+++ src/main/java/br/com/rodrigo/webflux/controllers/PlaylistController.java
@@ -1,6 +1,14 @@
 package br.com.rodrigo.webflux.controllers;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,27 +19,58 @@ import br.com.rodrigo.webflux.documents.Playlist;
 import br.com.rodrigo.webflux.services.PlaylistService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PlaylistController {
-
+	
 	@Autowired
 	PlaylistService service;
-
+	
 	@GetMapping(value="/playlist")
 	public Flux<Playlist> getPlaylist(){
 		return service.findAll();
 	}
-
-
+	
+	
 	@GetMapping(value="/playlist/{id}")
 	public Mono<Playlist> getPlaylistId(@PathVariable String id){
 		return service.findById(id);
 	}
-
+	
 	@PostMapping(value="/playlist")
 	public Mono<Playlist> save(@RequestBody Playlist playlist){
 		return service.save(playlist);
+	}
+	
+	@GetMapping(value="/playlist/webflux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Playlist>> getPlaylistByWebflux(){
+
+		System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+		Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> playlistFlux = service.findAll();
+
+        return Flux.zip(interval, playlistFlux);
+        
+	}
+
+	@GetMapping(value="/playlist/mvc")
+	public List<String> getPlaylistByMvc() throws InterruptedException {
+
+		System.out.println("---Start get Playlists by MVC--- " + LocalDateTime.now());
+
+
+		List<String> playlistList = new ArrayList<>();
+		playlistList.add("Java 8");
+		playlistList.add("Spring Security");
+		playlistList.add("Github");
+		playlistList.add("Deploy de uma aplicação java no IBM Cloud");
+		playlistList.add("Bean no Spring Framework");
+		TimeUnit.SECONDS.sleep(15);
+
+		return playlistList;
+
 	}
 
 }
--- src/main/java/br/com/rodrigo/webflux/webflux/WebfluxApplication.java
+++ src/main/java/br/com/rodrigo/webflux/webflux/WebfluxApplication.java
@@ -3,11 +3,19 @@ package br.com.rodrigo.webflux.webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
+		
+		Flux<String> colorFlux = Flux.just("black", "white", "blue");
+
+		colorFlux
+			.log()
+			.map(String::toUpperCase);
 	}
 
 }
